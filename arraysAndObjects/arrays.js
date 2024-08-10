57c3fa5ce65a56c87f25c89d5b6d1ab4267ac2aa
staran <staran@griddynamics.com>
2024-08-11 00:05:34 +0300

    :star: arrays and objects

---
+++ arraysAndObjects/arrays.js
@@ -0,0 +1,29 @@
+/*
+* Big O of Arrays
+*
+* Insertion - It depends... (push - O(1), unshift - O(N))
+* Removal - It depends... (pop - O(1), shift - O(N))
+* Searching - O(N)
+* Access - O(1)
+*
+* On unshift and shift, the indexes of the array have to be re-indexed.
+* */
+const names = ["Melissa", "Jenny", "Jessica", "Samantha", "Sara", "Amanda", "Katie", "Emily", "Ashley", "Brittany"];
+
+
+/*
+* Big O of Array methods
+*
+* push - O(1)
+* pop - O(1)
+* shift - O(N)
+* unshift - O(N)
+*
+* concat - O(N)
+* slice - O(N)
+* splice - O(N)
+*
+* sort - O(N * log N)
+*
+* forEach/map/filter/reduce/etc. - O(N)
+* */
